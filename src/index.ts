e0eb81070964e091619b3200c288145ec01b927e
Aberto <albertorolibert@gmail.com>
2021-01-08 10:39:59 -0500

    try 7

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import app from './app';
-import './database'
+import express from "express";
+import app from "./app";
+import "./database";
 
-app.get('/', (req, res) => {
-    res.send("hey budy")
-})
-app.listen(app.get('port'), ()=>{ 
-    console.log("server on port: ", app.get('port'))
-})
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("hey budy");
+});
+app.listen(app.get("port"), () => {
+  console.log("server on port: ", app.get("port"));
+});
